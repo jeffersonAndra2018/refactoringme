a9a5040ca00623f69030c9b8a48d3b5d29fa6995
William Suane <williams@backbase.com>
2019-09-20 20:08:04 +0200

    extra languages list

--- src/main/resources/init.sql
+++ src/main/resources/init.sql
@@ -2,3 +2,100 @@ INSERT INTO refactoringme.language (id, name)
 VALUES (1, 'Java');
 INSERT INTO refactoringme.language (id, name)
 VALUES (2, 'Javascript');
+INSERT INTO refactoringme.language (id, name)
+VALUES (2, 'C');
+INSERT INTO refactoringme.language (id, name)
+VALUES (4, 'Python');
+INSERT INTO refactoringme.language (id, name)
+VALUES (5, 'C++');
+INSERT INTO refactoringme.language (id, name)
+VALUES (6, 'Visual Basic .NET');
+INSERT INTO refactoringme.language (id, name)
+VALUES (7, 'C#');
+INSERT INTO refactoringme.language (id, name)
+VALUES (8, 'PHP');
+INSERT INTO refactoringme.language (id, name)
+VALUES (9, 'SQL');
+INSERT INTO refactoringme.language (id, name)
+VALUES (10, 'Objective-C');
+INSERT INTO refactoringme.language (id, name)
+VALUES (11, 'Assembly language');
+INSERT INTO refactoringme.language (id, name)
+VALUES (12, 'MATLAB');
+INSERT INTO refactoringme.language (id, name)
+VALUES (13, 'Perl');
+INSERT INTO refactoringme.language (id, name)
+VALUES (14, 'Object Pascal');
+INSERT INTO refactoringme.language (id, name)
+VALUES (15, 'R');
+INSERT INTO refactoringme.language (id, name)
+VALUES (16, 'Ruby');
+INSERT INTO refactoringme.language (id, name)
+VALUES (17, 'Visual Basic');
+INSERT INTO refactoringme.language (id, name)
+VALUES (18, 'Go');
+INSERT INTO refactoringme.language (id, name)
+VALUES (19, 'Groovy');
+INSERT INTO refactoringme.language (id, name)
+VALUES (20, 'Swift');
+
+INSERT INTO refactoringme.language (id, name)
+VALUES (21, 'PL/SQL');
+INSERT INTO refactoringme.language (id, name)
+VALUES (22, 'SAS');
+INSERT INTO refactoringme.language (id, name)
+VALUES (23, 'D');
+INSERT INTO refactoringme.language (id, name)
+VALUES (24, 'Lua');
+INSERT INTO refactoringme.language (id, name)
+VALUES (25, 'Dart');
+INSERT INTO refactoringme.language (id, name)
+VALUES (26, 'Fortran');
+INSERT INTO refactoringme.language (id, name)
+VALUES (27, 'COBOL');
+INSERT INTO refactoringme.language (id, name)
+VALUES (28, 'Scratch');
+INSERT INTO refactoringme.language (id, name)
+VALUES (29, 'Scala');
+INSERT INTO refactoringme.language (id, name)
+VALUES (30, 'ABAP');
+INSERT INTO refactoringme.language (id, name)
+VALUES (31, 'Lisp');
+INSERT INTO refactoringme.language (id, name)
+VALUES (32, 'Logo');
+INSERT INTO refactoringme.language (id, name)
+VALUES (33, 'Ada');
+INSERT INTO refactoringme.language (id, name)
+VALUES (34, 'Transact-SQL');
+INSERT INTO refactoringme.language (id, name)
+VALUES (35, 'Prolog');
+INSERT INTO refactoringme.language (id, name)
+VALUES (36, 'Scheme');
+INSERT INTO refactoringme.language (id, name)
+VALUES (37, 'Rust');
+INSERT INTO refactoringme.language (id, name)
+VALUES (38, 'Kotlin');
+INSERT INTO refactoringme.language (id, name)
+VALUES (39, 'Haskell');
+INSERT INTO refactoringme.language (id, name)
+VALUES (40, 'G');
+INSERT INTO refactoringme.language (id, name)
+VALUES (41, 'Julia');
+INSERT INTO refactoringme.language (id, name)
+VALUES (42, 'Tcl');
+INSERT INTO refactoringme.language (id, name)
+VALUES (43, 'PostScript');
+INSERT INTO refactoringme.language (id, name)
+VALUES (44, 'Ladder Logic');
+INSERT INTO refactoringme.language (id, name)
+VALUES (45, 'PL/I');
+INSERT INTO refactoringme.language (id, name)
+VALUES (46, 'Hack');
+INSERT INTO refactoringme.language (id, name)
+VALUES (47, 'Erlang');
+INSERT INTO refactoringme.language (id, name)
+VALUES (48, 'Standard ML');
+INSERT INTO refactoringme.language (id, name)
+VALUES (49, 'RPG');
+INSERT INTO refactoringme.language (id, name)
+VALUES (50, 'Bash');
